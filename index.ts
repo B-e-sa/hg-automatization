e7db3446a555aea8ef086e97291180b849075175
João Antonio Soares Pereira - 712210505 <712210505@ulife.com.br>
2023-10-04 20:56:44 -0300

    update: add sleep func after login and commentaries

--- index.ts
+++ index.ts
@@ -1,20 +1,29 @@
 import puppeteer from 'puppeteer';
+import * as dotenv from 'dotenv';
+import sleep from './utils/sleep';
 
-(async() => {
+dotenv.config();
+
+(async () => {
     const browser = await puppeteer.launch();
-
     const webpage = await browser.newPage();
 
-    await webpage.goto('https://dashboard.honeygain.com/login');
+    // GO TO HS LOGIN PAGE
+    await webpage.goto(String(process.env.HG_URL));
 
+    // ACCEPT COOKIES BUTTON
     await webpage.click('.khPLua');
 
-    await webpage.type('#email', process.env.USER_EMAIL as string);
-    await webpage.type('#password', process.env.USER_PASSWORD as string);
+    // FILLING CREDENTIALS
+    await webpage.type('#email', String(process.env.USER_EMAIL));
+    await webpage.type('#password', String(process.env.USER_PASSWORD));
 
-    await webpage.screenshot({
-        path: './temp/screenshot.png',
-    });
+    // LOGIN
+    await webpage.click('.hg-login-with-email')
 
+    // AWAIT PAGE LOAD
+    await sleep(10000);
+
+    // CLOSE BROWSER
     await browser.close();
 })();
