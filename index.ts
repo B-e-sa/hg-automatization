969560a9f3a9d590a9bf2f5a3781fa8550c91d5c
João Antonio Soares Pereira - 712210505 <712210505@ulife.com.br>
2023-10-04 21:07:51 -0300

    update: add try catch to main func

--- index.ts
+++ index.ts
@@ -1,29 +1,36 @@
 import puppeteer from 'puppeteer';
 import * as dotenv from 'dotenv';
-import sleep from './utils/sleep';
 
 dotenv.config();
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const webpage = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch();
+        const webpage = await browser.newPage();
 
-    // GO TO HS LOGIN PAGE
-    await webpage.goto(String(process.env.HG_URL));
+        // GO TO HS LOGIN PAGE
+        await webpage.goto(String(process.env.HG_URL));
 
-    // ACCEPT COOKIES BUTTON
-    await webpage.click('.khPLua');
+        // ACCEPT COOKIES BUTTON
+        await webpage.click('.khPLua');
 
-    // FILLING CREDENTIALS
-    await webpage.type('#email', String(process.env.USER_EMAIL));
-    await webpage.type('#password', String(process.env.USER_PASSWORD));
+        // FILLING CREDENTIALS
+        await webpage.type('#email', String(process.env.USER_EMAIL));
+        await webpage.type('#password', String(process.env.USER_PASSWORD));
 
-    // LOGIN
-    await webpage.click('.hg-login-with-email')
+        // LOGIN
+        await webpage.click('.hg-login-with-email');
 
-    // AWAIT PAGE LOAD
-    await sleep(10000);
+        // AWAIT PAGE LOAD
+        await webpage.waitForSelector(
+            // TODO: change to a real selector
+            'UNDEFINED', 
+            { timeout: 5000 },
+        );
 
-    // CLOSE BROWSER
-    await browser.close();
+        // CLOSE BROWSER
+        await browser.close();
+    } catch (e) {
+        throw new Error(e);
+    }
 })();
